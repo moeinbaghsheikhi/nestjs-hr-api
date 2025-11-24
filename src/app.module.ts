1eefb799b559c5675a96100e7390e8149d5af1c8
moein <moein.baghsheikhi@gmail.com>
2025-11-24 18:50:45 +0330

    departments module complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DepartmentsModule } from './modules/departments/departments.module';
 
 @Module({
   imports: [
+    // configModule for get env variable data
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env'
-    })
+    }),
+
+    // connect to database with typeorm
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: configService.get('DB_PORT', 3306),
+        username: configService.get('DB_USER', 'root'),
+        password: configService.get('DB_PASS', ''),
+        database: configService.get('DB_NAME', 'hr_db'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get('DB_SUNCHRONIZE', 'true') === 'true'
+      }),
+      inject: [ConfigService]
+    }),
+
+    // imports project modules
+    DepartmentsModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/modules/departments/departments.module.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Department } from "./entities/department.entity";
+import { Module } from "@nestjs/common";
+import { DepartmentsManagerController } from "./controllers/departments-manager.controller";
+import { DepartmentsManagerService } from "./services/departments-manager.service";
+import { DepartmentsEmployeeController } from "./controllers/departments-employee.controller";
+import { DepartmentsEmployeeService } from "./services/departments-employee.service";
+
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([Department])
+    ],
+    controllers: [DepartmentsManagerController, DepartmentsEmployeeController],
+    providers: [DepartmentsManagerService, DepartmentsEmployeeService]
+})
+
+export class DepartmentsModule {}
