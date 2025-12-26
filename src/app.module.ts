0c1959e85002dbba3c34d80969bcfad5797ae882
moein <moein.baghsheikhi@gmail.com>
2025-12-26 13:56:21 +0330

    change db to pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,9 +23,9 @@ import { PayrollModule } from './modules/payroll/payroll.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
+        type: 'postgres',
         host: configService.get('DB_HOST', 'localhost'),
-        port: configService.get('DB_PORT', 3306),
+        port: configService.get('DB_PORT', 5432),
         username: configService.get('DB_USER', 'root'),
         password: configService.get('DB_PASS', ''),
         database: configService.get('DB_NAME', 'hr_db'),
